5d23d74d51b3821c5d8429127d2267ce59010227
dhyces <pokmon987@gmail.com>
2022-11-21 21:59:28 -0500

    Initial

--- Fabric/src/main/java/dhyces/wartdecay/Config.java
+++
@@ -1,9 +0,0 @@
-package dhyces.wartdecay;
-
-import eu.midnightdust.lib.config.MidnightConfig;
-
-public class Config extends MidnightConfig {
-
-    @Comment public static Comment wartDropComment;
-    @Entry public static boolean doWartDrops = false;
-}
